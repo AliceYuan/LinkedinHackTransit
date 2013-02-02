c063f2803454a7cf6aed9129de4e9201608e15f5
Viktor Stanchev <me@viktorstanchev.com>
2013-02-02 02:13:51 -0500

    PRETTY COLORS!!11!eleven!

--- public/js/helper/util.js
+++ public/js/helper/util.js
@@ -1,8 +1,12 @@
 define(function(){
-  function makemap(lat, lon, w, h){
+  function makemap(locs, lat, lon, w, h){
+    var str = '';
+    for (var l in locs){
+      str += "&markers=color:"+locs[l].color+"%7Clabel:"+locs[l].letter+"%7C"+locs[l].lat+","+locs[l].lon;
+    }
     return "http://maps.googleapis.com/maps/api/staticmap?sensor=false&center="+
-    lat+","+lon+"&zoom=13&size="+w+"x"+h+
-    "&maptype=roadmap&markers=color:blue%7Clabel:S%7C"+lat+","+lon;
+    lat+","+lon+"&zoom=10&size="+w+"x"+h+
+    "&maptype=roadmap"+str;
   }
 
   return {"test": 1, makemap: makemap};
