ed4c943d96ac2e52db02bbba331a88c989a7f40d
Viktor Stanchev <me@viktorstanchev.com>
2013-02-01 23:01:57 -0500

    map demo

--- public/js/helper/util.js
+++ public/js/helper/util.js
@@ -1,6 +1,6 @@
 define(function(){
   function makemap(lat, lon, w, h){
-    return "http://maps.googleapis.com/maps/api/staticmap?center="+
+    return "http://maps.googleapis.com/maps/api/staticmap?sensor=false&center="+
     lat+","+lon+"&zoom=13&size="+w+"x"+h+
     "&maptype=roadmap&markers=color:blue%7Clabel:S%7C"+lat+","+lon;
   }
