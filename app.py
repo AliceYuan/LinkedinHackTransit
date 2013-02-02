450ad11b453ce77e55ad47777aede21ebadc6f45
Aaron Morais <aaron.m@live.ca>
2013-02-01 20:21:41 -0500

    Serve public folder files with the flask server at the endpoint /public

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 from flask import Flask, request, render_template, Response, url_for
+from werkzeug import SharedDataMiddleware
 
 app = Flask(__name__, template_folder='public')
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+  '/public': os.path.join(os.path.dirname(__file__), 'public')
+})
 
 @app.route('/')
 def root():
