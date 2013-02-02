3b729ee512457d8153329265237b8ba083538584
Alykhan Kanji <github@alykhan.com>
2013-02-02 04:53:54 -0500

    Also return stops with no predictions from ttc_times.py.

--- ttc_times.py
+++ ttc_times.py
@@ -9,15 +9,18 @@ def getUpcomingDepartures(latitude, longitude):
 	departures = []
 	nearbyStops = json.loads(getNearbyStops(latitude, longitude))["Stops"]
 	for stop in nearbyStops:
+		dictionary = {}
 		if 'stopId' in stop:
 			predictions = requestNextBusData('predictions', 'stopId='+stop["stopId"])
+			# print predictions
 			rootPredictions = ET.fromstring(predictions)
 			for predictions in rootPredictions.findall('predictions'):
 				if len(predictions.findall('direction')) > 0:
-					dictionary = {}
 					for direction in predictions.findall('direction'):
 						for prediction in direction.findall('prediction'):
 							dictionary.update({'route': predictions.attrib["routeTag"], 'stop': predictions.attrib["stopTitle"], 'minutes': prediction.attrib["minutes"]})
-							departures.append(dictionary)
+				else:
+					dictionary.update(predictions.attrib)
+		departures.append(dictionary)
 	data = { "Departures" : departures }
 	return data
