d3b35106040d443bbc32ec50a242103c91663cf8
Viktor Stanchev <me@viktorstanchev.com>
2013-02-02 13:07:28 -0500

    cache expires

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import json
 from flask import Flask, request, render_template, Response, url_for, jsonify, send_from_directory
 from werkzeug import SharedDataMiddleware
 from ttc_request import getPredictions
+from time import time
 
 cache = None
+last_cached = 0
 
 app = Flask(__name__, template_folder='public')
 app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
@@ -19,10 +21,12 @@ def root():
 @app.route('/get', methods = ['GET']) 
 def getStops():
     global cache
+    global last_cached
     lat = request.args["lat"] 
     lon = request.args["lon"]
-    if cache is None:
+    if cache is None or time() - last_cached > 5*60:
         cache = createResponse(lat,lon)
+        last_cached = time()
     return jsonify(cache)
 
 def createResponse(lat,lon):
