eca090dde0c074366e0abb4549901564384ee53a
Aaron Morais <aaron.m@live.ca>
2013-02-02 12:18:46 -0500

    Output realtime json at /get endpoint

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import json
 from flask import Flask, request, render_template, Response, url_for, jsonify, send_from_directory
 from werkzeug import SharedDataMiddleware
+from ttc_request import getPredictions
 
 app = Flask(__name__, template_folder='public')
 app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
@@ -21,26 +22,27 @@ def getStops():
     return jsonify(resp)
 
 def createResponse(lat,lon):
-    upcoming = ttc_times.getUpcomingDepartures(float(lat),float(lon))
+    upcoming = getPredictions(float(lat), float(lon))
+    upcoming = json.loads(upcoming)
     stops = []
-#   for stop in upcoming["Departures"]:
-#       if stop:
-#           route = createRoute("TTC", stop["route"], stop["minutes"])
-#           stop = createStop(stop["lat"], stop["lon"], [route])
-#           stops.append(stop)
-#   responseJSON = {"stops" : stops}
-#   return responseJSON
+    for stop in upcoming["Predictions"]:
+        if stop:
+            route = createRoute(stop[1], stop[4], stop[6], stop[5])
+            stop = createStop(stop[2], stop[3], [route])
+            stops.append(stop)
+    responseJSON = {"stops" : stops}
+    return responseJSON
 
 def createStop(lat, lon, routes):
     return {'lat':lat,'lon':lon, 'routes':routes}
 
-def createRoute(routeType, name, times):
-    return {'type':routeType, 'name':name, 'times':times}
+def createRoute(routeType, name, times, direction):
+    return {'type':routeType, 'name':name, 'times':times, 'direction':direction}
  
 def createTimes(times):
     return times
 
 if __name__ == '__main__':
-#print createResponse("43.7196699","-79.4012199")
+    print createResponse("43.7196699","-79.4012199")
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
