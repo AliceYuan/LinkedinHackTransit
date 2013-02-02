66f98139a0179ee306dc3bea03f3e0d35fe5f1b4
Aaron Morais <aaron.m@live.ca>
2013-02-01 21:01:36 -0500

    Add data.json sample. Update root of site to be served without templating. Add /getStops endpoint that serves basic json.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template, Response, url_for
+from flask import Flask, request, render_template, Response, url_for, jsonify, send_from_directory
 from werkzeug import SharedDataMiddleware
 
 app = Flask(__name__, template_folder='public')
@@ -9,7 +9,17 @@ app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
 
 @app.route('/')
 def root():
-    return render_template('test.html')
+    return send_from_directory('public', 'test.html')
+
+@app.route('/getStops', methods = ['GET']) 
+def getStops():
+    routes = [
+       {'lat': 1, 
+        'lon': 2, 
+        'type':""
+       }
+    ]
+    return jsonify(results = routes)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
