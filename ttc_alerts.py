416e250d4fc27bd6561d64b3f1bb8b62da56b4b7
Alykhan Kanji <github@alykhan.com>
2013-02-04 00:30:47 -0500

    Add ttc_alerts.py, getAlerts() returns list of current TTC issues.

---
+++ ttc_alerts.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup
+
+def getAlerts():
+	index_html = requests.get('http://ttc.ca/Service_Advisories/all_service_alerts.jsp')
+	soup = BeautifulSoup(index_html.text)
+	advisory_wrap = soup.find(class_="advisory-wrap")
+	alerts = []
+	for alert_content in advisory_wrap.find_all('div'):
+		alerts.append((alert_content.find(class_="veh-replace").string, alert_content.find(class_="alert-updated").string))
+	return alerts
+
+if __name__=="__main__":
+	print getAlerts()
