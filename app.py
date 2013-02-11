16e4bfdfb80c1ef0b6f76d3622299cef5c2976a7
Alykhan Kanji <github@alykhan.com>
2013-02-11 18:45:49 -0500

    Update response logic to use new data from ttc.py.

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask import Flask, request, render_template, Response, url_for, jsonify, s
 from werkzeug import SharedDataMiddleware
 import ttc
 
-cache = None
-
 app = Flask(__name__, template_folder='public')
 app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
   '/': os.path.join(os.path.dirname(__file__), 'public')
@@ -13,49 +11,50 @@ app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
 
 @app.route('/')
 def root():
-    return send_from_directory('public', 'index.html')
+	return send_from_directory('public', 'index.html')
 
-@app.route('/get', methods = ['GET']) 
+@app.route('/get', methods = ['GET'])
 def getStops():
-    global cache
-    lat = request.args["lat"] 
-    lon = request.args["lon"]
-    if cache is None:
-        cache = createResponse(lat,lon)
-    return jsonify(cache)
-
-@app.route('/update')
-def update():
-    global cache
-    cache = None
-    return "updated cache"
+	lat = request.args["lat"]
+	lon = request.args["lon"]
+	return jsonify(createResponse(lat,lon))
 
 def createResponse(lat,lon):
-    upcoming = ttc.getPredictions(float(lat), float(lon))
-    upcoming = json.loads(upcoming)
-    stops = collections.OrderedDict()
-    for stop in upcoming["Predictions"]:
-        if stop:
-            route = createRoute(stop[1], stop[6], stop[5])
-            stopend = createStop(stop[2], stop[3], stop[4], [route])
-            if str(stop[2])+str(stop[3]) not in stops:
-                stops[str(stop[2])+str(stop[3])] = stopend
-            else:
-                val = stops[str(stop[2]) + str(stop[3])]
-                val['routes'].append(route)
-                stops[str(stop[2])+str(stop[3])] = val
-    resp = []
-    for k,v in stops.items():
-        resp.append(v)
-    responseJSON = {"stops" : resp}
-    return responseJSON
+	predictions = json.loads(ttc.getPredictions(float(lat), float(lon)))
+	# uniqueStops = collections.OrderedDict()
+	stops = []
+	for stop in predictions['predictions']:
+		dList = []
+		for direction in stop['directions']:
+			directionDict = {}
+			for dirName,details in direction.iteritems():
+				mList = []
+				for prediction in details:
+					mList.append(prediction['minutes'])
+			directionDict.update({'direction': dirName, 'type': stop['routeTag'], 'times': mList})
+		dList.append(directionDict)
+		stops.append({'name': stop['stopTitle'], 'lat': stop['lat'], 'lon': stop['lon'], 'routes': dList})
+		# route = createRoute(stop['routeTag'], [0], stop['routeTitle'])
+		# stopend = createStop(stop['lat'], stop['lon'], stop['stopTitle'], dList)
+		# if str(stop['lat'])+str(stop['lon']) not in uniqueStops:
+		# 	uniqueStops.update({str(stop['lat'])+str(stop['lon']): stopend})
+		# else:
+		# 	val = uniqueStops[str(stop['lat']) + str(stop['lon'])]
+		# 	val['routes'].append(route)
+		# 	uniqueStops.update({str(stop['lat'])+str(stop['lon']): val})
+	# resp = []
+	# for k,v in uniqueStops.items():
+	# 	resp.append(v)
+	responseJSON = { 'stops' : stops }
+	return responseJSON
 
 def createStop(lat, lon, intersection, routes):
-    return {'lat':lat,'lon':lon, 'name':intersection, 'routes':routes}
+	return {'lat':lat,'lon':lon, 'name':intersection, 'routes':routes}
 
 def createRoute(routeTag, times, direction):
-    return {'type':routeTag, 'times':times, 'direction':direction}
- 
+	return {'type':routeTag, 'times':times, 'direction':direction}
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
+	# print json.dumps(createResponse(43.7739, -79.41427), indent=4)
